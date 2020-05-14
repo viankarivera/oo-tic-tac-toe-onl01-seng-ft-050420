3b76c67930b69cad8179e1915db3f2d7f902fdfd
Vianka Rivera <viankalrivera@gmail.com>
2020-05-14 15:33:28 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,45 @@
+class TicTacToe
+  def initialize(board = nil) #making a new board
+    @board = board || Array.new(9, " ") #new board will create an array with 9 possibilities
+  end
+
+  WIN_COMBINATIONS = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
+
+  def move(position, token = "X")
+    @board[position] = token
+  end
+
+  def position_taken?(index) #if the board position is occupied return true, otherwise let them move
+    !(@board[index].nil? || @board[index] == " ")
+  end
+
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(index)
+  end
+
+  def turn_count
+
+  end
+
+end
