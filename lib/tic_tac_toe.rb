ed8a783c1d8ac715013b125d15e103d12a48476d
Vianka Rivera <viankalrivera@gmail.com>
2020-05-14 15:56:14 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -60,5 +60,14 @@ end
 
 def turn
   puts "Please enter 1-9"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(index)
+    player_token = current_player
+    move(index, player_token)
+    display_board
+  else
+    turn
+  end
 
 end
